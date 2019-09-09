611186927022dc68789bebed8a43c0a3f43c1908
c00kamunga <tsothalanti2@yahoo.com>
2019-09-09 15:16:57 -0700

    finished array.methods and callsbacks

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -40,25 +40,79 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
 
 function getLength(arr, cb) {
+  return cb(arr.length);
   // getLength passes the length of the array into the callback.
 }
+const grocery = getLength(items, stuff => {
+  return stuff;
+})
+console.log(grocery);
+
+
+
+
 
 function last(arr, cb) {
+  return cb(arr[arr.length-1])
   // last passes the last item of the array into the callback.
 }
+const gross = last(items, stuff => {
+  return stuff;
+})
+console.log(gross);
 
-function sumNums(x, y, cb) {
+
+
+
+
+
+function sumNums(x, y, cb) {;
+  return cb(x+y)
   // sumNums adds two numbers (x, y) and passes the result to the callback.
 }
+const addsTwo = sumNums(9,5,math => {
+  return math;
+})
+console.log(addsTwo);
 
-function multiplyNums(x, y, cb) {
+
+
+
+
+
+function multiplyNums(x, y, cb) {;
+  return cb(x*y);
   // multiplyNums multiplies two numbers and passes the result to the callback.
 }
+const multipliesTwo = multiplyNums(5, 4, multiplies => {
+  return multiplies;
+})
+console.log(multipliesTwo)
+
+
+
+
+
 
 function contains(item, list, cb) {
+  if(list.includes(item)){
+    return cb(true);
+  }
+  else{
+    return cb(false);
+  }
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
 }
+const checker = contains('Pencil', items, stuff => {
+  return  stuff;
+})
+console.log(checker);
+
+
+
+
+
 
 /* STRETCH PROBLEM */
 
